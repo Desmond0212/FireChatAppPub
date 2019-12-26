4ac27cc71e9e2c48c21bbba275f231b3517e2431
Desmond Wong <desmond.9602@gmail.com>
2019-12-26 14:13:42 +0800

    Initial Commit

---
+++ FireChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireChatApp
+//
+//  Created by DesmondWong on 26/12/2019.
+//  Copyright © 2019 DesmondWong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
